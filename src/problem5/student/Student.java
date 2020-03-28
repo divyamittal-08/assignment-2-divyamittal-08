059f76f03290a5e3617ded2c972a58930bde8941
Divya Mittal <divyamittal790@gmail.com>
2020-03-28 19:36:36 +0530

    created fields of Student and constructor

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,14 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private int rollno;
+    private String name;
+    private int backlog;
+    private int count;
+    public Student(int rollno, String name, int backlog) {
+        this.rollno = rollno;
+        this.name = name;
+        this.backlog = backlog;
+        count=0;
+    }
 }

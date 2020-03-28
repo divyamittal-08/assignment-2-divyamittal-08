33d20420da51b7d43537f73de1d0204a9aab3599
Divya Mittal <divyamittal790@gmail.com>
2020-03-28 19:51:44 +0530

    created setter getter

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -16,4 +16,18 @@ public class Node {
         this.data = data;
         next=null;
     }
+    public Student getData() {
+        return data;
+    }
+    public void setData(Student data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }

a7f928808120bec0bb2b8ac41dc7f6c111ea12f9
Divya Mittal <divyamittal790@gmail.com>
2020-03-28 17:19:25 +0530

    created insertElement function

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -16,7 +16,44 @@ public class MyBinarySearchTree {
         head = null;
         comp = null;
     }
-
+    public void insertElement(int val){
+        TreeNode temp = new TreeNode(val);
+        if(head == null){
+            temp.setLeft(null);
+            temp.setRight(null);
+            temp.setKey(val);
+            head = temp;
+            comp = temp;
+        }
+        else{
+            while(comp != temp) {
+                if (comp.getKey() > val) {
+                    if (comp.getRight() == null) {
+                        comp.setRight(temp);
+                        temp.setKey(val);
+                        temp.setRight(null);
+                        temp.setLeft(null);
+                        comp = temp;
+                        break;
+                    } else {
+                        comp.setRight(comp);
+                    }
+                } else if (comp.getKey() < val) {
+                    if (comp.getLeft() == null) {
+                        comp.setLeft(temp);
+                        temp.setKey(val);
+                        temp.setLeft(null);
+                        temp.setRight(null);
+                        comp = temp;
+                        break;
+                    } else {
+                        comp.setLeft(comp);
+                    }
+                }
+            }
+        }
+        comp = head;
+    }
 
 
 

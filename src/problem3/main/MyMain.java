0a981ed3ec4328b64ae311d019cffc9112d9cb28
Divya Mittal <divyamittal790@gmail.com>
2020-03-28 19:02:44 +0530

    created object of MyPriorityQueue in main class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,16 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyPriorityQueue obj = new MyPriorityQueue();
+    }
 }

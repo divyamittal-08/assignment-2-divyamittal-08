b8178b148a2438ecff522d492adce4965c236377
Divya Mittal <divyamittal790@gmail.com>
2020-03-28 20:01:08 +0530

    created main function

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,18 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue obj = new MyCircularQueue();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Press 1 for enqueue, press 2 for dequeue:- ");
+        int counter = sc.nextInt();
+        sc.nextLine();
+    }
 }

29b090689cb0c8ca71def8f0783cbe5bb9811ccc
Divya Mittal <divyamittal790@gmail.com>
2020-03-28 17:09:36 +0530

    created getter setter.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,5 +10,33 @@ public class TreeNode {
     private TreeNode right;
     private TreeNode left;
     private int key;
+    public TreeNode(int val){
+        this.key = val;
+        left = right = null;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
 
 }
